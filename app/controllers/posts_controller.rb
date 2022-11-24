a90c8b12bcdd3035ec3eb0711dcc53724c7273a9
Denly09 <denniely.garcia09@gmail.com>
2022-11-24 16:38:45 +0800

    add actopn create and new with error message form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.includes(:user).all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+    private
+
+    def post_params
+      params.require(:post).permit(:original_url)
+  end
 end
