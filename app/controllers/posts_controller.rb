3e9441e472a040a1f2350dd1d3993dcf58b808f2
Denly09 <denniely.garcia09@gmail.com>
2022-11-24 19:21:11 +0800

    add show action and send_to_url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.includes(:user).all
@@ -6,19 +7,34 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    sprintf "%04d", rand(2 - 9999), unique: true
   end
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
-      redirect_to posts_path
+      redirect_to @post
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def show
+    @post =Post.find(params[:id])
+    url_extension = @post.id.to_s(36)
+    @short_url = "http://localhost:3000/#{url_extension}"
+  end
+
+  def send_to_url
+    id = params[:short_url].to_i(36)
+    post = Post.find(id)
+    redirect_to post.original_url
+  end
+
     private
 
     def post_params
       params.require(:post).permit(:original_url)
-  end
+    end
 end
